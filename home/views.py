265f2667016ebb91f2eb3b7e8e647e545511a4a1
ranjanpandey984 <ranjanpandey984@gmail.com>
2021-08-12 19:17:19 +0545

    Slider Cat SubCat uploaded

--- home/views.py
+++ home/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import *
+
 # Create your views here.
 
-class BaseView(View):
-	view = {}
+class BaseView(View): # parameter ko view chahi generic view ho
+	views = {}
 
 
 
 class HomeView(BaseView):
 	def get(self,request):
-
-		return render(request,'index.html')
+		self.views['sliders'] = Slider.objects.all()
+		self.views['categories'] = Category.objects.all()
+		self.views['subcategories'] = SubCategory.objects.all()
+		self.views['items'] = Product.objects.filter(status = 'active')
+		return render(request,'index.html',self.views)
